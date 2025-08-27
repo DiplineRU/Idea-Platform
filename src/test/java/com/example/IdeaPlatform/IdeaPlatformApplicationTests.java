05f43747c8dacc1733e6e301dfe20733a8dbb296
DiplineRU <artgul52@gmail.com>
2025-08-27 15:51:45 +0300

    init commit

---
+++ src/test/java/com/example/IdeaPlatform/IdeaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IdeaPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
